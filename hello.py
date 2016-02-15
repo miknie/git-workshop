68bdfb36107c8e5f3599928b6295ec8394a976f1
Mikael Niemi <mikael@niemi.se>
2016-02-15 01:46:05 +0100

    Added Good Bye message

--- hello.py
+++ hello.py
@@ -2,19 +2,25 @@ import vcs
 from vcs_values import the_value_of
 
 HELLO_MSG = 'Helo and welcome!'
+GOOD_BYE_MSG = 'Good by!'
 
 
 def main():
+
     print(HELLO_MSG)
 
     git = the_value_of(vcs.GIT)
     svn = the_value_of(vcs.SVN)
 
     print('And the perfered VCS is...')
+
     if svn > git:
-        assert(false)
+        assert False
     else:
         print('Git :)')
+
+    print(GOOD_BYE_MSG)
+
 
 if __name__ == '__main__':
     main()
